d9c91b14c81b23961ebba773d56459f276bf5439
Leonardo Adams <leonardoadams@gmail.com>
2019-07-27 15:56:37 -0300

    Add style to BooksListView title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import BooksListView from './BooksListView'
+import './App.css'
 
 /**
 * @description Main class that start the app and has the books state
