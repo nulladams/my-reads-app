e9f1ab8a1d44eed2c17a6050b3eab13f584d1f20
Leonardo Adams <leonardoadams@gmail.com>
2019-07-27 23:02:15 -0300

    Create update api function

--- src/utils/BooksAPI.js
+++ src/utils/BooksAPI.js
@@ -21,3 +21,12 @@ export const getAll = () =>
         .then(res => res.json())
         .then(data => data.books)
 
+export const update = (book, shelf) =>
+    fetch(`${api}/books/${book.id}`, {
+        method: 'PUT',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ shelf })
+    }).then(res => res.json())
