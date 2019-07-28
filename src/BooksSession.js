1ba07919413eafe61b066586ba725ffd86cdc3f1
Leonardo Adams <leonardoadams@gmail.com>
2019-07-27 23:50:02 -0300

    Create BooksSession component

---
+++ src/BooksSession.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class BooksSession extends Component {
+    render() {
+        const { shelfSession, books, onChangeBookShelf } = this.props
+        return(
+            <div className="bookshelf-books">
+
+            </div>
+        )
+    }
+}
+
+export default BooksSession
+
