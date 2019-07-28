177865ba6e4d27ee02d9f67e3996039b8102b54d
Leonardo Adams <leonardoadams@gmail.com>
2019-07-27 23:03:17 -0300

    Create search api function

--- src/utils/BooksAPI.js
+++ src/utils/BooksAPI.js
@@ -30,3 +30,14 @@ export const update = (book, shelf) =>
         },
         body: JSON.stringify({ shelf })
     }).then(res => res.json())
+
+export const search = (query, maxResults) =>
+    fetch(`${api}/search`, {
+        method: 'POST',
+        headers: {
+            ...headers,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ query, maxResults })
+    }).then(res => res.json())
+        .then(data => data.books)
