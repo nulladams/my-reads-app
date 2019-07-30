9230d4f54cf1fb79257828379725f53a3b7bae86
Leonardo Adams <leonardoadams@gmail.com>
2019-07-29 23:26:49 -0300

    Create Book component

---
+++ src/Book.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+/**
+* @description Represents a Book
+* @constructor
+*/
+class Book extends Component {
+    render() {
+        return(
+            <li>
+
+            </li>
+        )
+    }
+}
+
+export default Book
