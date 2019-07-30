5c76a4209c70603ada4efcffb127604b9a97ecda
Leonardo Adams <leonardoadams@gmail.com>
2019-07-29 23:22:51 -0300

    Show books infos according its session

--- src/BooksSession.js
+++ src/BooksSession.js
@@ -5,6 +5,13 @@ class BooksSession extends Component {
         const { shelfSession, books, onChangeBookShelf } = this.props
         return(
             <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books.filter(book => book.shelf === shelfSession)
+                        .map(book => (
+                            <li>{book.title}</li>
+                        ))
+                    }
+                </ol>
             </div>
         )
     }
