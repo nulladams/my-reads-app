61cf1efaaf7fda5c089ebd95ae8c58e61a4b1d06
Leonardo Adams <leonardoadams@gmail.com>
2019-07-30 22:38:45 -0300

    Create BookShelfChanger component

---
+++ src/BookShelfChanger.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+/**
+* @description Change the shelf where the book is located or remove it from the reads 
+* @constructor
+*/
+class BookShelfChanger extends Component {
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default BookShelfChanger
