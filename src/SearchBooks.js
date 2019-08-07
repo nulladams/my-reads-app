d9ec9f4077a2a89994d3e99c6228ee0c35189317
Leonardo Adams <leonardoadams@gmail.com>
2019-08-07 20:13:27 -0300

    Create SearchBooks component

---
+++ src/SearchBooks.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+/**
+* @description View that have the search input and search results components
+* @constructor
+*/
+class SearchBooks extends Component {
+    render() {
+        return(
+            <div>
+                Search
+            </div>
+        )
+    }
+}
+
+export default SearchBooks
