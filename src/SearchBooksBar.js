a9c7e84ba040769ec8f67f6124f05df915049e11
Leonardo Adams <leonardoadams@gmail.com>
2019-08-08 20:55:22 -0300

    Add close search link

--- src/SearchBooksBar.js
+++ src/SearchBooksBar.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class SearchBooksBar extends Component {
     render() {
         return(
             <div className="search-books-bar">
-                
+                <Link
+                    to='/'
+                    className="close-search"
+                >Close</Link>
             </div>
         )
     }
