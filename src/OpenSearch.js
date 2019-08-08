9dcbb2dee6de2eda22ee9e7f50822b3e8b606a6f
Leonardo Adams <leonardoadams@gmail.com>
2019-08-08 20:36:44 -0300

    Add style to open search link

--- src/OpenSearch.js
+++ src/OpenSearch.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 class OpenSearch extends Component {
     render() {
         return(
-            <div>
+            <div className="open-search">
                 <Link
                     to='/search'
                 >Add Book</Link>
